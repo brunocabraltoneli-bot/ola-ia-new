4ad1fb9240c3d9131f362757c16cfe65aae3867a
[dyad] <git@dyad.sh>
2026-04-29 21:28:43 -0300

    [dyad] Atualizando nome do desenvolvedor no rodapé para Bruno Cabral - wrote 2 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,7 +2,7 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center text-sm text-gray-500 dark:text-gray-400">
       <p>
-        Desenvolvido por <span className="font-medium text-indigo-600 dark:text-indigo-400">Seu Nome</span>
+        Desenvolvido por <span className="font-medium text-indigo-600 dark:text-indigo-400">Bruno Cabral</span>
       </p>
       <a
         href="https://www.dyad.sh/"
