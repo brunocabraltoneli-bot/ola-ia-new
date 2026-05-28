7f19751987b8966755a3ce3b2a85594075e8896a
[dyad] <brunocabraltoneli@gmail.com>
2026-05-28 17:19:27 -0300

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,1 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Chat from "./pages/Chat";
-import Tasks from "./pages/Tasks";
-
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/tasks" element={<Tasks />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
-
-export default App;
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; import Index from "./pages/Index"; import NotFound from "./pages/NotFound"; import Chat from "./pages/Chat"; import Tasks from "./pages/Tasks"; import Navbar from "@/components/ui/navbar"; const App = () => ( <BrowserRouter> <Navbar /> <Routes> <Route path="/" element={<Index />} /> <Route path="/chat" element={<Chat />} /> <Route path="/tasks" element={<Tasks />} /> <Route path="*" element={<NotFound />} /> </Routes> </BrowserRouter> ); export default App;
