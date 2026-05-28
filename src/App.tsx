c0d2825033ee29adde4665c0133fb488078e3e1c
[dyad] <brunocabraltoneli@gmail.com>
2026-05-28 16:20:48 -0300

    [dyad] Added Tasks page with CRUD functionality, mobile-first design, and Supabase integration. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Chat from "./pages/Chat";
+import Tasks from "./pages/Tasks";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/chat" element={<Chat />} />
+          <Route path="/tasks" element={<Tasks />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
