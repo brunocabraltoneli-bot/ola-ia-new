63d7a0a6fde076c6c648e74bac1c29afddce9bcd
[dyad] <brunocabraltoneli@gmail.com>
2026-06-11 19:59:03 -0300

    [dyad] Fixed App.tsx: root shows Login when not authenticated, protected /chat and /tasks with PrivateRoute - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,28 @@ import NotFound from "./pages/NotFound";
 import Tasks from "./pages/Tasks";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
+import { useAuthContext } from "./contexts/AuthContext";
 
 export default function App() {
+  const { user, loading } = useAuthContext();
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        {/* Root route - shows Login if not authenticated */}
-        <Route path="/" element={<Login />} />
+        {/* Root route: show Login if not authenticated, else Index */}
+        <Route
+          path="/"
+          element={
+            loading ? (
+              <div className="p-4">Loading...</div>
+            ) : user ? (
+              <Index />
+            ) : (
+              <Login />
+            )
+          }
+        />
 
         {/* Protected routes */}
         <Route
