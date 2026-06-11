5ffb994dc3b6c805b5d5e9861e0a85d08add8bfd
[dyad] <brunocabraltoneli@gmail.com>
2026-06-11 19:49:08 -0300

    [dyad] Restored Login as the main route and fixed PrivateRoute usage - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,10 @@ export default function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Index />
-            </PrivateRoute>
-          }
-        />
+        {/* Root route - shows Login if not authenticated, otherwise shows Index */}
+        <Route path="/" element={<Login />} />
 
-        <Route path="/login" element={<Login />} />
-
+        {/* Protected routes */}
         <Route
           path="/home"
           element={
@@ -52,6 +45,68 @@ export default function App() {
             </PrivateRoute>
           }
         />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+</arg_value>
+
+<dyad-command type="restart"></dyad-command>
+<dyad-chat-summary>Restored Login as the main route and fixed PrivateRoute usage</dyad-chat-summary>
+<dyad-write path="src/App.tsx" description="Fix routing - Login should be at root, PrivateRoute only for Home and Tasks">
+"use client";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Home from "./pages/Home";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Tasks from "./pages/Tasks";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/* Root route - shows Login if not authenticated */}
+        <Route path="/" element={<Login />} />
+
+        {/* Protected routes */}
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/tasks"
+          element={
+            <PrivateRoute>
+              <Tasks />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
