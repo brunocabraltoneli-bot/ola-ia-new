cd626009e7434fb93d6758fd9d624a81cf39bba3
[dyad] <brunocabraltoneli@gmail.com>
2026-06-11 22:24:45 -0300

    [dyad] Restaurando App.tsx para exibir tela principal sem bloqueio de login - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,16 @@ import PrivateRoute from "./components/PrivateRoute";
 import { useAuthContext } from "./contexts/AuthContext";
 import AuthRedirector from "./components/AuthRedirector";
 
-const ProtectedHome = () => {
-  const { user } = useAuthContext();
-  
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-  
-  return (
-    <Layout>
-      <Index />
-    </Layout>
-  );
-};
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProtectedHome />} />
-        <Route path="/home" element={<Layout><PrivateRoute><Index /></PrivateRoute></Layout>} />
+        <Route path="/" element={<Index />} />
+        <Route path="/home" element={<Layout><Index /></Layout>} />
         <Route path="/chat" element={<Layout><Chat /></Layout>} />
         <Route path="/tarefas" element={<Layout><Tasks /></Layout>} />
-        <Route path="/login" element={<AuthRedirector><Login /></AuthRedirector>} />
-        <Route path="/entrar" element={<AuthRedirector><Login /></AuthRedirector>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/entrar" element={<Login />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
