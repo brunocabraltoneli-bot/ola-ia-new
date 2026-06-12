c21c3208ee45302f9eb8becbfaeff82df8e88041
[dyad] <brunocabraltoneli@gmail.com>
2026-06-11 21:34:29 -0300

    [dyad] Created Layout component with Navbar and wrapped protected routes in App.tsx - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-"import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Index from "./pages/Index";
 import Chat from "./pages/Chat";
 import Tasks from "./pages/Tasks";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/entrar" element={<Login />} />
-        <Route path="/home" element={<Index />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/tarefas" element={<Tasks />} />
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Index />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/tarefas" element={<Tasks />} />
+        </Route>
         <Route path="*" element={<Navigate to="/entrar" replace />} />
       </Routes>
     </BrowserRouter>
